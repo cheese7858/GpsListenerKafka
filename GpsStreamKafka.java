b1e0f0a85d871df13b63ec17875c69144c0c826f
daolin <s4380597@uq.edu.au>
2021-11-01 11:56:32 +1030

    added GpsStream; can remove the Altitude field; can select latitude between 39.5-40.5 and longitude between 115.5-117.0; can send topic named Beijing; can send topic SimpleTrackerX

---
+++ GpsStreamKafka.java
@@ -0,0 +1,75 @@
+/**
+ *******************************************************************************
+ * @file GpsListenerKafka/GpsStreamKafka.java
+ * @author Daolin Chen - a1838238
+ * @date 30102021
+ * @brief Use kafka stream to perform transformations on the events from the 
+ * producer instance
+ *******************************************************************************
+ */
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.processor.TopicNameExtractor;
+import org.apache.kafka.streams.processor.internals.StaticTopicNameExtractor;
+
+import javax.sound.sampled.Line;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+public class GpsStreamKafka {
+
+    public static void main(String[] args) {
+
+        // out put topic
+        String simpleTrackerOutput = "SimpleTracker";  
+        List<String> simpleTrackerInput = Arrays.asList("Tracker0", "Tracker1", "Tracker2", "Tracker3", "Tracker4", "Tracker5",
+                "Tracker6", "Tracker7", "Tracker8", "Tracker9");
+        Properties pro = new Properties();
+        pro.put(StreamsConfig.APPLICATION_ID_CONFIG,"logProcessor");
+        pro.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,"192.168.244.128:9092");
+
+        // Serdes for converting an object into a stream of bytes and vice versa
+        Serde<String> serde = Serdes.String();
+        StreamsBuilder builder1 = new StreamsBuilder();
+        KStream<String, String> stream = builder1.stream(simpleTrackerInput, Consumed.with(serde, serde));
+        // use KStream to split the stream into single string
+        KStream<String, String> stream1 = stream.mapValues(
+                line -> line.split(",")[0]+","+line.split(",")[1]);
+        // output serialization/deserialization to results
+        stream1.to(simpleTrackerOutput, Produced.with(serde, serde));
+        // init and start KStream
+        KafkaStreams streams = new KafkaStreams(builder1.build(),pro);
+        streams.start();
+
+        // output to topic
+        String output = "Beijing";  
+        List<String> input = Arrays.asList("Tracker0", "Tracker1", "Tracker2", "Tracker3", "Tracker4", "Tracker5",
+                "Tracker6", "Tracker7", "Tracker8", "Tracker9");
+        Properties properties = new Properties();
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG,"logProcessor");
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,"192.168.244.128:9092");
+        Serde<String> stringSerde = Serdes.String();
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> simpleFirstStream = builder.stream(input, Consumed.with(stringSerde, stringSerde));
+        KStream<String, String> upperCasedStream = simpleFirstStream.filter(
+                (k, line) -> Double.parseDouble(line.split(",")[0])>=39.5 && Double.parseDouble(line.split(",")[0])<=40.5)
+                .filter((k, line)->Double.parseDouble(line.split(",")[1])>=115.5 && Double.parseDouble(line.split(",")[1])<=117.0);
+
+        upperCasedStream.to(output, Produced.with(stringSerde, stringSerde));
+
+        KafkaStreams kafkaStreams = new KafkaStreams(builder.build(), properties);
+        kafkaStreams.start();
+
+
+
+    }
+
+}
+
