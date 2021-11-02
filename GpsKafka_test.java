a053c5a60554550f3d93ca2f832b246fc55a9455
daolin <s4380597@uq.edu.au>
2021-11-02 20:39:41 +1030

    Added test cases; fixed file name

---
+++ GpsKafka_test.java
@@ -0,0 +1,55 @@
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.Scanner;
+
+
+public class GpsKafka_test {
+
+    @Before
+    public void test1(){
+        //properties
+        Properties props = new Properties();
+
+        //initiate kafka server 
+        props.put("bootstrap.servers", "192.168.244.128:9092");
+        props.put("group.id", "test");
+
+        //set strings as key/value pairs
+        props.put("key.deserializer", StringDeserializer.class);
+        props.put("value.deserializer", StringDeserializer.class);
+        props.put("session.timeout.ms", 6000);
+        props.put("heartbeat.interval.ms", 2000);
+
+        //initiate consumer
+        KafkaConsumer<String,String> consumer = new KafkaConsumer<>(props);
+        //subscribe to topic
+        System.out.println("Please submit the Tracker Number：");
+        Scanner scanner = new Scanner(System.in);
+        String name = "Tracker1";
+        consumer.subscribe(Arrays.asList(name));
+        System.out.println("Start subscribe"+name);
+        
+        //read records from server
+        while (true){
+            ConsumerRecords<String,String> records = consumer.poll(Duration.ofMillis(1000));
+            for(ConsumerRecord<String,String> record : records){
+                System.out.println(name+"'s output:  " + record.value());
+            }
+        }
+    }
+
+    @Test
+    public void test2(){
+        GpsService service = new GpsService();
+        service.run();
+    }
+
+}
